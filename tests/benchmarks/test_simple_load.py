7a43984db33cf249d7d4febecb28411b1cb13717
J. Nick Koston <nick@koston.org>
2025-02-21 10:41:01 -0600

    chore: add simple benchmark

--- tests/benchmarks/test_simple_load.py
+++ tests/benchmarks/test_simple_load.py
@@ -4,15 +4,15 @@ import pathlib
 
 from pytest_codspeed import BenchmarkFixture
 
-from annotatedyaml import load_yaml
+from annotatedyaml import parse_yaml
 
 FIXTURE_PATH = pathlib.Path(__file__).parent.parent.joinpath("fixtures")
 
 
-def test_simple_load(benchmark: BenchmarkFixture) -> None:
-    """Test loading a simple YAML file."""
+def test_simple_parse_yaml(benchmark: BenchmarkFixture) -> None:
+    """Test parsing a simple YAML file."""
     yaml_str = FIXTURE_PATH.joinpath("simple_automations.yaml").read_text()
 
     @benchmark
-    def _load_yaml() -> None:
-        load_yaml(yaml_str)
+    def _parse_yaml() -> None:
+        parse_yaml(yaml_str)
