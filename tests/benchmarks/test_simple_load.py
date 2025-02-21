58ae1a83fb22b13bd88d8414b7c7d88a512fa493
J. Nick Koston <nick@koston.org>
2025-02-21 10:44:34 -0600

    chore: add codspeed benchmarks (#6)

---
+++ tests/benchmarks/test_simple_load.py
@@ -0,0 +1,18 @@
+"""Benchmarks."""
+
+import pathlib
+
+from pytest_codspeed import BenchmarkFixture
+
+from annotatedyaml import parse_yaml
+
+FIXTURE_PATH = pathlib.Path(__file__).parent.parent.joinpath("fixtures")
+
+
+def test_simple_parse_yaml(benchmark: BenchmarkFixture) -> None:
+    """Test parsing a simple YAML file."""
+    yaml_str = FIXTURE_PATH.joinpath("simple_automations.yaml").read_text()
+
+    @benchmark
+    def _parse_yaml() -> None:
+        parse_yaml(yaml_str)
