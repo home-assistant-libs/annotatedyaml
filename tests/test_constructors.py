250ea3b71e4a1bc27f0b1d84efbc0ebb161d6a12
J. Nick Koston <nick@koston.org>
2025-03-18 11:45:08 -1000

    chore: add cover

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import io
+from unittest.mock import Mock
+
 from yaml.nodes import ScalarNode
 
 from annotatedyaml.constructors import _handle_scalar_tag
@@ -9,10 +12,19 @@ from annotatedyaml.objects import NodeStrClass
 
 def test__handle_scalar_tag_non_str():
     """Test _handle_scalar_tag with a non-str scalar."""
-    loader = PythonSafeLoader()
+    loader = PythonSafeLoader(io.BytesIO(b""))
     node = ScalarNode("tag:yaml.org,2002:int", 123)
+    node_str_class = _handle_scalar_tag(loader, node)
+    assert not isinstance(node_str_class, NodeStrClass)
+    assert node_str_class == 123
+
+
+def test__handle_scalar_tag_str():
+    """Test _handle_scalar_tag with a str scalar."""
+    loader = PythonSafeLoader(io.BytesIO(b""))
+    node = ScalarNode("tag:yaml.org,2002:str", "123", Mock(line=3))
     node_str_class = _handle_scalar_tag(loader, node)
     assert isinstance(node_str_class, NodeStrClass)
     assert node_str_class == "123"
-    assert node_str_class.__config_file__ == "<string>"
+    assert node_str_class.__config_file__ == "<file>"
     assert node_str_class.__line__ == node.start_mark.line + 1
