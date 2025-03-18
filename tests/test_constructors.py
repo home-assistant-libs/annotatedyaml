9db29d2a84039fa3d8a54805aac1590eebf1a1f3
J. Nick Koston <nick@koston.org>
2025-03-18 11:48:15 -1000

    chore: add cover

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -28,3 +28,14 @@ def test__handle_scalar_tag_str():
     assert node_str_class == "123"
     assert node_str_class.__config_file__ == "<file>"
     assert node_str_class.__line__ == node.start_mark.line + 1
+
+
+def test__handle_scalar_tag_str_missing_startmark():
+    """Test _handle_scalar_tag with a str scalar with no startmark."""
+    loader = PythonSafeLoader(io.BytesIO(b""))
+    node = ScalarNode("tag:yaml.org,2002:str", "123")
+    node_str_class = _handle_scalar_tag(loader, node)
+    assert isinstance(node_str_class, NodeStrClass)
+    assert node_str_class == "123"
+    assert node_str_class.__config_file__ == "<file>"
+    assert not hasattr(node_str_class, "__line__")
