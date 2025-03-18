21c93519340e20315ae829c1be324661e87af98f
J. Nick Koston <nick@koston.org>
2025-03-18 00:15:40 -1000

    fix: export node classes at top level
    pylint cannot see the cythonized versions but it should
    be able to see the top level ones
    
    https://github.com/home-assistant/core/pull/140861#issuecomment-2732539715

--- src/annotatedyaml/__init__.py
+++ src/annotatedyaml/__init__.py
@@ -5,11 +5,14 @@ from .dumper import dump, save_yaml
 from .exceptions import YAMLException, YamlTypeError
 from .input import UndefinedSubstitution, extract_inputs, substitute
 from .loader import Secrets, load_yaml, load_yaml_dict, parse_yaml, secret_yaml
-from .objects import Input
+from .objects import Input, NodeDictClass, NodeListClass, NodeStrClass
 
 __all__ = [
     "SECRET_YAML",
     "Input",
+    "NodeDictClass",
+    "NodeListClass",
+    "NodeStrClass",
     "Secrets",
     "UndefinedSubstitution",
     "YAMLException",
