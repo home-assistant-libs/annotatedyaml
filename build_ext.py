0f9b168865616234892ac1dec0d34ebc3afed704
J. Nick Koston <nick@koston.org>
2025-08-01 09:06:56 -1000

    chore: drop Python 3.12, add Python 3.14 (#52)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- build_ext.py
+++ build_ext.py
@@ -2,8 +2,9 @@
 
 import logging
 import os
-from distutils.command.build_ext import build_ext
 from typing import Any
+
+from distutils.command.build_ext import build_ext
 
 try:
     from setuptools import Extension
